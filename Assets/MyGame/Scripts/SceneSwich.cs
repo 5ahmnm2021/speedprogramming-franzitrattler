534560dbd430f5091b915df5ac46090be6373f98
franzitrattler <franziska.trattler@gmail.com>
2021-04-13 09:07:44 +0200

    create SceneSwitches

---
+++ Assets/MyGame/Scripts/SceneSwich.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneSwich : MonoBehaviour
+{
+  public void LoadScene1()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void LoadScene2()
+    {
+        Application.LoadLevel(2); 
+    }
+}
